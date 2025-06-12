970d61f28bd1766b6d92fafdf6540a4a3ae6eaac
Malkin15 <mal.kincz@gmail.com>
2025-06-12 19:38:22 +0200

    Update Task_2.sql

--- Task_2.sql
+++ Task_2.sql
@@ -2,24 +2,20 @@
  období v dostupných datech cen a mezd? */
 
 SELECT
-	a.pay_year,
-	a.branch_name,
-	a.avg_payroll,
-	b.cat_name,
-	b.avg_price,
-	ROUND(a.avg_payroll / (b.avg_price::numeric), 0) AS buy_power
+  b.pay_year AS year,
+  b.cat_name AS product,
+  ROUND(AVG(b.avg_price), 2) AS avg_price_value,
+  CASE 
+    WHEN b.cat_name = 'Chléb konzumní kmínový' THEN 'kg'
+    WHEN b.cat_name = 'Mléko polotučné pasterované' THEN 'l'
+    ELSE ''
+  END AS price_unit,
+  ROUND(AVG(a.avg_payroll / b.avg_price), 2) AS avg_buy_power
 FROM t_roman_simik_project_sql_primary_final a
 JOIN t_roman_simik_project_sql_primary_final b
-	ON a.pay_year = b.pay_year
+  ON a.pay_year = b.pay_year
 WHERE b.cat_name IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
-	AND a.branch_name IS NOT NULL
-	AND
-	(a.pay_year = (
-	SELECT MIN(pay_year)
-	FROM t_roman_simik_project_sql_primary_final)
-		OR a.pay_year = (
-	SELECT MAX(pay_year)
-	FROM t_roman_simik_project_sql_primary_final)
-	)
-ORDER BY a.pay_year;
-
+  AND a.branch_name IS NOT NULL
+  AND a.pay_year IN (2006, 2018)
+GROUP BY b.pay_year, b.cat_name
+ORDER BY b.pay_year, b.cat_name;
